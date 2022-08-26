f8c822b1d93fd814add6a4e7a28f6de335231162
bjorn <bjorn@swis.nl>
2022-08-26 11:55:12 +0200

    wsl crontab fix

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -16,3 +16,5 @@ chmod -f 644 $HOME/.ssh/id_rsa.pub || :
 chmod -f 644 $HOME/.ssh/authorized_keys || :
 
 grep -qxF "'alias k='kubectl'" $HOME/.bashrc || echo "alias k='kubectl'" >> $HOME/.bashrc
+
+echo "WSL FIX in crontab: */5 * * * * sudo bash -c 'find /run/WSL/ -maxdepth 1 -type s -mmin +5 -name *_interop | xargs rm'"
